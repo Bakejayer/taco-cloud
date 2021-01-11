03ab900062fac63bdb00dc0c3a9dca76c0676c1b
Jacob Bayer <jbbuilder8@yahoo.com>
2021-01-11 00:18:46 -0500

    Set home view using a Java config class and removed the home controller

--- src/test/java/com/bayer/tacocloud/HomeControllerTest.java
+++ src/test/java/com/bayer/tacocloud/HomeControllerTest.java
@@ -1,6 +1,5 @@
 package com.bayer.tacocloud;
 
-import com.bayer.tacocloud.controller.HomeController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,15 +7,12 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-
 import static org.hamcrest.Matchers.containsString;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(HomeController.class)
+@WebMvcTest()
 public class HomeControllerTest {
 
     @Autowired

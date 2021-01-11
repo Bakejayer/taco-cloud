99513a5a659ff31721b20911ee58e2daf75affc5
Jacob Bayer <jbbuilder8@yahoo.com>
2021-01-10 20:49:00 -0500

    Added Model, controller, and view objects to facilitate designing and ordering a taco

---
+++ src/main/java/com/bayer/tacocloud/model/Ingredient.java
@@ -0,0 +1,17 @@
+package com.bayer.tacocloud.model;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+public class Ingredient {
+
+    private final String id;
+    private final String name;
+    private final Type type;
+
+    public static enum Type{
+        WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
+    }
+}
